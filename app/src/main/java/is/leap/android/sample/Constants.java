071cd87211d71bf44e71cf8b0f27cfdd9fa703ce
arnoldlaishram <arnold@jiny.io>
2021-04-05 16:15:31 +0530

    Removed unused code

--- app/src/main/java/is/leap/android/sample/Constants.java
+++
@@ -1,7 +0,0 @@
-package is.leap.android.sample;
-
-public class Constants {
-
-    public static final String LEAP = "LEAP";
-    public static final String OWNER = "owner";
-}
--- app/src/main/java/is/leap/android/sample/listeners/ValidationListener.java
+++
@@ -1,8 +0,0 @@
-package is.leap.android.sample.listeners;
-
-import org.json.JSONObject;
-
-public interface ValidationListener {
-    void onSuccessfulValidation(JSONObject config);
-    void onFailedValidation();
-}
