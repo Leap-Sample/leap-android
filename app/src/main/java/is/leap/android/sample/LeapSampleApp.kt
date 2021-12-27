6750afe5135f547b95ff13b2cd29868b6d058b3a
ashish-jiny <ashish@jiny.io>
2021-12-27 17:33:20 +0530

    mixpanel code added

--- app/src/main/java/is/leap/android/sample/LeapSampleApp.kt
+++ app/src/main/java/is/leap/android/sample/LeapSampleApp.kt
@@ -1,5 +1,6 @@
 package `is`.leap.android.sample
 
+import `is`.leap.android.LeapEventCallbacks
 import `is`.leap.android.aui.Leap
 import `is`.leap.android.creator.LeapCreator
 import android.app.Application
@@ -7,9 +8,10 @@ import `is`.leap.android.sample.data.LeapSampleSharedPref
 import `is`.leap.android.sample.receiver.SampleAppReceiver
 import android.content.IntentFilter
 import android.util.Log
+import com.mixpanel.android.mpmetrics.MixpanelAPI
+import org.json.JSONObject
 
 class LeapSampleApp : Application() {
-
 
 
     override fun onCreate() {
@@ -18,12 +20,23 @@ class LeapSampleApp : Application() {
         Leap.start("44a8633d-2c31-4ade-8535-6ebd39b8b262")
         LeapCreator.start("44a8633d-2c31-4ade-8535-6ebd39b8b262")
 
-        Leap.setLeapEventCallbacks {
-            Log.d("LeapCallbacks", "" + it.keys)
-        }
+        val mixpanel = MixpanelAPI.getInstance(this, "a6665720e68a9a1458e7ae6fa11a55d1")
+
+        Leap.setLeapEventCallbacks(LeapEventCallbacks() {
+//            override fun onEvent(p0: MutableMap<String, String>?) {
+ 
+            val jsonObject = JSONObject(it as Map<String, String>?)
+            Log.d("LeapCallbacks", "JSON: $jsonObject")
+            mixpanel.track(it?.get("eventName") + "_r", jsonObject)
+            mixpanel.flush()
+            //Log.d("LeapCallbacks", "" + it?.keys + "\n" + it?.values)
+//            }
+        })
 
         LeapSampleSharedPref.init(this)
         initialiseReceiver(this)
+
+
     }
 
     private fun initialiseReceiver(application: Application) {
