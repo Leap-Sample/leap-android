fe71f3bc6d847975a1a21102d8ed1d9cd0405ffe
ashish-jiny <ashish@jiny.io>
2021-12-29 11:09:00 +0530

    gradle version updated to 7.0.4 and Java version to 11

--- app/src/main/java/is/leap/android/sample/LeapSampleApp.kt
+++ app/src/main/java/is/leap/android/sample/LeapSampleApp.kt
@@ -1,13 +1,12 @@
 package `is`.leap.android.sample
 
-import `is`.leap.android.LeapEventCallbacks
 import `is`.leap.android.aui.Leap
-import `is`.leap.android.creator.LeapCreator
 import android.app.Application
 import `is`.leap.android.sample.data.LeapSampleSharedPref
 import `is`.leap.android.sample.receiver.SampleAppReceiver
 import android.content.IntentFilter
 import android.util.Log
+import android.widget.Toast
 import com.mixpanel.android.mpmetrics.MixpanelAPI
 import org.json.JSONObject
 
@@ -17,26 +16,24 @@ class LeapSampleApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Leap.start("44a8633d-2c31-4ade-8535-6ebd39b8b262")
-        LeapCreator.start("44a8633d-2c31-4ade-8535-6ebd39b8b262")
+        Leap.withBuilder("44a8633d-2c31-4ade-8535-6ebd39b8b262")
+            .addProperty("test", "test_value")
+            .start()
+//        LeapCreator.start("44a8633d-2c31-4ade-8535-6ebd39b8b262")
 
         val mixpanel = MixpanelAPI.getInstance(this, "a6665720e68a9a1458e7ae6fa11a55d1")
 
-        Leap.setLeapEventCallbacks(LeapEventCallbacks() {
-//            override fun onEvent(p0: MutableMap<String, String>?) {
- 
-            val jsonObject = JSONObject(it as Map<String, String>?)
+        Leap.setLeapEventCallbacks {
+            val jsonObject = JSONObject(it as Map<String, String>)
             Log.d("LeapCallbacks", "JSON: $jsonObject")
-            mixpanel.track(it?.get("eventName") + "_r", jsonObject)
+            Toast.makeText(this, "LeapCallbacks for " + it["eventName"], Toast.LENGTH_SHORT)
+                .show()
+            mixpanel.track(it["eventName"] + "_r", jsonObject)
             mixpanel.flush()
-            //Log.d("LeapCallbacks", "" + it?.keys + "\n" + it?.values)
-//            }
-        })
+        }
 
         LeapSampleSharedPref.init(this)
         initialiseReceiver(this)
-
-
     }
 
     private fun initialiseReceiver(application: Application) {
